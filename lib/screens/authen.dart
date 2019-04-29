726a76498e926298796ffdc1ef38ebb0a755bcd1
jesada <jesada.h@cdg.co.th>
2019-04-29 14:11:06 +0700

    create Widget showappname

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,8 +6,20 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+
+
+  Widget showappname(){
+    return Text('this is CHODE');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('StatefulWidget');
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 100.0),
+        alignment: Alignment(0, -1),
+        child: showappname(),
+      ),
+    );
   }
 }
