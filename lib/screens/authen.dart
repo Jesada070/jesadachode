74e9696b792661c1782af73af4f640064cdf9303
jesada <jesada.h@cdg.co.th>
2019-04-29 15:33:57 +0700

    color  Gradient

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,19 +6,37 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showappname() {
+    return Text(
+      'Register',
+      style: TextStyle(
+          fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.red,fontFamily: 'Kalam'),
+    );
+  }
 
-
-  Widget showappname(){
-    return Text('this is CHODE');
+  Widget showlogo() {
+    return Image.asset('images/logo2.gif');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 100.0),
+      body: Container(decoration : BoxDecoration(gradient: LinearGradient(colors: [Colors.yellow,Colors.green],begin: Alignment(-1, 1))),
+        padding: EdgeInsets.only(top: 60.0),
         alignment: Alignment(0, -1),
-        child: showappname(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 200.0,
+              height: 200.0,
+              child: showlogo(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15.0),
+              child: showappname(),
+            )
+          ],
+        ),
       ),
     );
   }
